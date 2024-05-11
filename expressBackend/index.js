1702223719ff7b53e4f2a5cf1934a6f878933786
ademola adeniji <aademola750@gmail.com>
2024-05-11 19:00:31 +0100

    stage

--- expressBackend/index.js
+++ expressBackend/index.js
@@ -4,22 +4,12 @@ const cors = require('cors');
 const user = require("./routes/userRoute.js");
 const blog = require("./routes/blogRoute.js");
 const mongoose = require('mongoose');
-const path = require('path')
 
 
 
-
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
-
-const buildPath = path.join(__dirname, 'build')
-
-//Get static files
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(buildPath, 'index.html'))
-
-})
 
 app.use(express.json());
 
@@ -28,7 +18,7 @@ app.use('/', user);
 app.use('/', blog)
 
 
-mongoose.connect(process.env.LOCALDB_URI).then(
+mongoose.connect(process.env.MONGODB_URI).then(
     ()=>{
         app.listen(PORT, ()=>{
             console.log("App listening on port:", PORT)
