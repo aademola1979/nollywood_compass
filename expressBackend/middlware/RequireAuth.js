41ecd399d0450bd1700608e5f934fc0c40b2453b
ademola adeniji <aademola750@gmail.com>
2024-05-11 19:03:13 +0100

    stage

--- expressBackend/middlware/RequireAuth.js
+++ expressBackend/middlware/RequireAuth.js
@@ -14,7 +14,7 @@ const requireAuth = async(req, res, next) =>{
     try {
         const {_id} = jwt.verify(token, process.env.SECRET)
 
-        res.user = await User.findOne({_id}).select('_isAdmin')
+        res.user = await User.findOne({_id}).select('_id')
         next()
 
     } catch (error) {
